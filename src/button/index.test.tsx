f81924f323c5be574cde8b5fda074f8dd207d477
jaredfrank <2761906353@qq.com>
2026-03-29 15:56:34 +0800

    2.0增加了button的test版本

--- src/button/index.test.tsx
+++ src/button/index.test.tsx
@@ -1,10 +1,29 @@
 // import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Button from './index';
 
-test('renders button', () => {
-  render(<Button>click me</Button>);
-  const linkElement = screen.getByText(/click me/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Button',()=>{
+  test('renders button', () => {
+    render(<Button onClick={()=>{}} onBlur={()=>{}}>click me</Button>);
+    const buttonElement = screen.getByText(/click me/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('renders button with type', () => {
+    render(<Button type="primary" onClick={()=>{}} onBlur={()=>{}}>click me</Button>);
+    const buttonElement = screen.getByText(/click me/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('renders button with size', () => {
+    render(<Button size="large" onClick={()=>{}} onBlur={()=>{}}>click me</Button>);
+    const buttonElement = screen.getByText(/click me/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('should support click',()=>{
+    const onClick = vi.fn();
+    render(<Button onClick={onClick} type='primary' onBlur={()=>{}}>click me</Button>);
+    const linkElement =screen.getByText(/click me/i)
+    fireEvent.click(linkElement);
+    expect(onClick).toHaveBeenCalled();
+  })
+})
--- src/button/index.tsx
+++ src/button/index.tsx
@@ -8,14 +8,18 @@ interface ButtonProps{
   children:React.ReactNode;
   backgroundColor?:string;
   style?:React.CSSProperties;
+  size?:'large'|'small'|'medium';
+  onClick?:React.MouseEventHandler<HTMLButtonElement>;
+  onBlur?:React.FocusEventHandler<HTMLButtonElement>;
 }
 const Button =(props:ButtonProps)=>{
-  const {className,type='normal',children,style}=props;
+  const {className,type='normal',children,style,size='medium',onClick,onBlur}=props;
   const cls=classNames({
     'ant-btn':true,
+    [`ant-btn-${size}`]: size,
     [`ant-btn-${type}`]: type,  // 使用模板字符串
     [className as string]:className
   })
-  return <button className={cls} style={style}>{children}</button>
+  return <button className={cls} style={style} onClick={onClick} onBlur={onBlur}>{children}</button>
 }
 export default Button;
